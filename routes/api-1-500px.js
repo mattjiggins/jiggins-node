bf4037d3c14471866ae59d9b563afbd1c08301ec
Matthew Jiggins <githubpublic@jiggins.ca>
2016-02-18 17:28:01 +0100

    basic caching
    - really should cache the twitter and 500px feeds! “Cacheman" to the
    rescue.

--- routes/api-1-500px.js
+++ routes/api-1-500px.js
@@ -7,33 +7,55 @@ var API500 = require('600px'),
 		token: process.env.FIVEHUNDRED_TOKEN,
 		token_secret: process.env.FIVEHUNDRED_TOKEN_SECRET		
 	});
+	
+var Cacheman = require('cacheman');
+var cacheFiveHundred = new Cacheman({ ttl: 600 }); // 10 minutes	
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	api500.photos.getByUsername('mattjiggins',{sort:'taken_at',image_size:'200,440,1600'})
-		.catch(console.error)
-		.then(
-			function(results) {
-				var thePhotos = results.photos;
-				var photos = []
+	
+	cacheFiveHundred.get('fivehundred', function (err, data) {		
+		if (err) throw err;
 
-				thePhotos.forEach(function(image){
-					photos.push({
-						id: image.id,
-						name: image.name,
-						taken_at: image.taken_at,
-						description: image.description,
-						url: image.url,
-						width: image.width,
-						height: image.height,
-						img_200: image.images[0].url,
-						img_440: image.images[1].url,
-						img_1600: image.images[2].url
-					})
+		if(! data){
+			console.log("No 500, getting sum.");
+			
+			api500.photos.getByUsername('mattjiggins',{sort:'taken_at',image_size:'200,440,1600'})
+			.catch(console.error)
+			.then(
+				function(results) {
+					var thePhotos = results.photos;
+					var photos = []
+
+					thePhotos.forEach(function(image){
+						photos.push({
+							id: image.id,
+							name: image.name,
+							taken_at: image.taken_at,
+							description: image.description,
+							url: image.url,
+							width: image.width,
+							height: image.height,
+							img_200: image.images[0].url,
+							img_440: image.images[1].url,
+							img_1600: image.images[2].url
+						})
+					});
+					
+  				  	cacheFiveHundred.set ('fivehundred', photos);
+					
+			        res.send(photos);
 				});
-				
-	            res.send(photos);
-			});
+		} else {
+			console.log("500, sending.");
+			// Use the data we have.
+			res.send( data );
+		};
+	});
+	
+	
+	
+
 });
 
 module.exports = router;
--- routes/api-1-twitter.js
+++ routes/api-1-twitter.js
@@ -9,16 +9,37 @@ var client = new Twitter({
 	access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
+var Cacheman = require('cacheman');
+var cacheTweets = new Cacheman({ ttl: 600 }); // 10 minutes
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	var params = {screen_name: 'mattjiggins'};
-	client.get('statuses/user_timeline', params, function(error, tweets, response){
-	  if (!error) {
-		  res.send(tweets);
-	  } else {
-		  res.send(error);
-	  }
+	
+	cacheTweets.get('tweets', function (err, data) {		
+		if (err) throw err;
+
+		if(! data){
+			console.log("No tweets, getting sum.");
+			
+			// There is no data, do this function which will check for perm cache etc
+			var params = {screen_name: 'mattjiggins'};
+			client.get('statuses/user_timeline', params, function(error, tweets, response){
+			  if (!error) {
+				  cacheTweets.set ('tweets', tweets);
+				  res.send(tweets);
+			  } else {
+				  res.send(error);
+			  }
+			});
+			
+		} else {
+			console.log("Tweets, sending.");
+			// Use the data we have.
+			res.send( data );
+		};
 	});
+	
+
 });
 
 module.exports = router;
